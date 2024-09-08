47fdb22189593c55d7abf1f75b9abf4a947cf49d
takoyummy <takoyummy@naver.com>
2024-09-08 14:06:45 +0900

    chore: init commit

---
+++ src/main/java/com/example/TestCodePracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.testcodepractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestCodePracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestCodePracticeApplication.class, args);
+	}
+
+}
